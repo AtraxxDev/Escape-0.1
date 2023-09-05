dbaddf9630907a8354b60d6b46d2ff357be53093
JoseJulioJim <cdmx2115@amerike.edu.mx>
2023-09-05 15:53:44 -0600

    Script luces

---
+++ Assets/Scripts/JoseJulion/SliderFunction.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderFunction : MonoBehaviour
+{
+    public Light pointlight;
+    public Light pointlight2;
+    public Light pointlight3;
+    public Light pointlight4;
+
+    public void OnValueChanged(float value)
+    {   
+        if (pointlight != null)
+        {
+            pointlight.intensity = value;
+        }
+        if (pointlight2 != null)
+        {
+            pointlight2.intensity = value;
+        }
+        if (pointlight3 != null)
+        {
+            pointlight3.intensity = value;
+        }
+        if (pointlight4 != null)
+        {
+            pointlight4.intensity = value;
+        }
+    }
+}
