8fa22fa61d6f51270fd9a6165c77218be4d4fea6
Sr Almkeja <cdmx1895@amerike.edu.mx>
2023-10-23 16:34:03 -0600

    Frasquitos listos falta código

--- Assets/Scripts/Ivan/ShowPW.cs
+++ Assets/Scripts/Ivan/ShowPW.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ShowPW : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //[SerializeField] private TMP_Text Text;
+    public GameObject CaptainLogObj;
+
+    public string codigoAsignado; // Nuevo campo para el código asignado
+
+    // Este método se llama al inicio para asignar un código específico.
+    public void AsignarCodigo(string codigo)
+    {
+        codigoAsignado = codigo;
+       // Text.text = codigoAsignado;
+    }
     void Start()
     {
-        
+
+        codigoAsignado += Random.Range(1, 9) + "";
+        codigoAsignado += Random.Range(1, 9) + "";
+        codigoAsignado += Random.Range(1, 9) + "";
+        codigoAsignado += Random.Range(1, 9) + "";
+
+
+        AsignarCodigo(codigoAsignado);
+        Debug.Log(" " + codigoAsignado);
+
     }
 
     // Update is called once per frame
