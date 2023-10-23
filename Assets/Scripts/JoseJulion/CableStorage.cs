2d5f7ca2308d033c78bbd35b5dc6362558d8f1f3
MasterPedro1 <cdmx2115@amerike.edu.mx>
2023-10-23 16:21:02 -0600

    Cables añadidos

--- Assets/Scripts/JoseJulion/CableStorage.cs
+++ Assets/Scripts/JoseJulion/CableStorage.cs
@@ -1,40 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CableStorage : MonoBehaviour
 {
-    bool redCable = false;
-    bool blueCable = false;
-    bool greenCable = false;
-    bool pinkCable = false;
-    bool purpleCable = false;
+    public CableStorage OtherHand;
+    public UnityEvent OnActivated;
+    public bool redCable = false;
+    public bool blueCable = false;
+    public bool greenCable = false;
+    public bool pinkCable = false;
+    public bool purpleCable = false;
+    public bool InteractWith = false;
 
-    public void OnCollisionEnter(Collision collision)
+    public void SetActivation(bool value)
+    {
+        if ((redCable = true) && (blueCable = true) && (greenCable = true) && (pinkCable = true) && (purpleCable = true))
+        {
+            InteractWith = value;
+            OnActivated.Invoke();
+        }
+    }
+
+    public void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("redCable"))
         {
             redCable = true;
+            OtherHand.redCable = true;
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.CompareTag("blueCable"))
         {
             blueCable = true;
+            OtherHand.blueCable = true;
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.CompareTag("greenCable"))
         {
             greenCable = true;
+            OtherHand.greenCable = true;
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.CompareTag("pinkCable"))
         {
             pinkCable = true;
+            OtherHand.pinkCable = true;
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.CompareTag("purpleCable"))
         {
             purpleCable = true;
+            OtherHand.purpleCable = true;
             Destroy(collision.gameObject);
         }
     }
