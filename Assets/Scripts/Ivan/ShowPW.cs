a9639938e69df0564fe469b49a722c3f9fe97857
Tobias Gtz <cdmx1895@amerike.edu.mx>
2023-10-30 16:26:55 -0600

    Wuuuh Wuuuh

--- Assets/Scripts/Ivan/ShowPW.cs
+++ Assets/Scripts/Ivan/ShowPW.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 public class ShowPW : MonoBehaviour
 {
     //[SerializeField] private TMP_Text Text;
-    //public GameObject CaptainLogObj;
+    public GameObject CaptainLogObj;
 
     public string codigoAsignado; // Nuevo campo para el código asignado
 
     // Este método se llama al inicio para asignar un código específico.
     public void AsignarCodigo(string codigo)
     {
-        codigoAsignado = codigo;
-       // Text.text = codigoAsignado;
+        //codigoAsignado = codigo;
+        //Text.text = ;
     }
     void Start()
     {
@@ -22,17 +22,16 @@ public class ShowPW : MonoBehaviour
         codigoAsignado += 5 + "";
         codigoAsignado += 1 + "";
         codigoAsignado += 8 + "";
-        //codigoAsignado += Random.Range(1, 9) + "";
-
-
-        AsignarCodigo(codigoAsignado);
-        Debug.Log(" " + codigoAsignado);
 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Activate()
     {
-        
+        CaptainLogObj.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        CaptainLogObj.SetActive(!true);
     }
 }
