76a2c44c0a823b9572ac8a8b0f43dfba0a96103d
Tobias Gtz <cdmx1895@amerike.edu.mx>
2023-11-06 15:10:09 -0600

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Ivan/GasesChad.cs
+++ Assets/Scripts/Ivan/GasesChad.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class GasesChad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public float MaxFrasco1=8; //4,2,6-Meta
+    public float MaxFrasco2=6; //4,6,2
+    public float MaxFrasco3=4; //3,4
+
+    public float recieverFlask=4;
+    public float senderFlask=4;
+    public float emptyFlask=0;
+
+
+    private void Update()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Fill(int Flasktype,float amount)
     {
-        
+        switch(Flasktype)
+        {
+            case 1:
+                recieverFlask= Mathf.Clamp(recieverFlask+amount,0,MaxFrasco1); 
+                break;
+            case 2:
+                senderFlask = Mathf.Clamp(recieverFlask + amount, 0, MaxFrasco1);
+                break;
+            case 3:
+                emptyFlask = Mathf.Clamp(recieverFlask + amount, 0, MaxFrasco1);
+                break;
+        }
+    }
+
+    public void Empty()
+    {
+
     }
 }
