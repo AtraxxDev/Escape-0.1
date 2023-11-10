08bca74034ce84f4168a3536d273d46d9e9e77ef
Ivan PM <cdmx2125@amerike.edu.mx>
2023-11-10 13:15:36 -0600

    Turn Off Lever casi listo, falta animaciones

--- Assets/Scripts/JoseJulion/TurnOnOffLever.cs
+++ Assets/Scripts/JoseJulion/TurnOnOffLever.cs
@@ -4,24 +4,117 @@ using UnityEngine;
 public class TurnOnOffLever: MonoBehaviour
 {
     public List<Light> lightList;
-    public void Activation()
+    private bool Light1;
+    private bool Light2;
+    private bool Light3;
+    private bool Light4;
+    private bool Activate1=true;
+    private bool Activate2=true;
+    private bool Activate3 = true;
+    private bool Activate4 = true;
+
+    /* public void Activation()
+     {
+         //RECORRE LA LISTA
+         for(int i = 0; i < lightList.Count; i++) 
+         {
+             //CHECA SI EXISTE
+             if (lightList[i] != null) 
+             {
+                 //FLIP
+                 if (lightList[i].enabled)
+                 {
+                     lightList[i].enabled = false;
+                 }
+                 else
+                 {
+                     lightList[i].enabled = true;
+                 }
+             }
+         }
+     }*/
+
+    public void Activation1()
     {
-        //RECORRE LA LISTA
-        for(int i = 0; i < lightList.Count; i++) 
+        if(Activate1==true)
         {
-            //CHECA SI EXISTE
-            if (lightList[i] != null) 
-            {
-                //FLIP
-                if (lightList[i].enabled)
-                {
-                    lightList[i].enabled = false;
-                }
-                else
-                {
-                    lightList[i].enabled = true;
-                }
-            }
+            Activate1 = false;
+            lightList[0].enabled = true;
+            Light1 = true;
+            lightList[1].enabled = true;
+            Light2 = true;
         }
+        else
+        {
+            Activate1 = true;
+            lightList[0].enabled = false;
+            Light1 = false;
+            lightList[1].enabled = false;
+            Light2 = false;
+        }
+     
+    }
+
+    public void Activation2()
+    {
+        if(Activate2==true)
+        {
+            Activate2 = false;
+            lightList[2].enabled = true;
+            Light3 = true;
+            lightList[1].enabled = false;
+            Light2 = false;
+        }
+        else
+        {
+            Activate2 = true;
+            lightList[2].enabled = false;
+            Light3 = false;
+            lightList[1].enabled = true;
+            Light2 = true;
+        }
+    }
+
+    public void Activation3()
+    {
+        if(Activate3==true)
+        {
+            Activate3 = false;
+            lightList[3].enabled = true;
+            Light4 = true;
+            lightList[2].enabled = false;
+            Light3 = false;
+        }
+        else
+        {
+            Activate3 = true;
+            lightList[3].enabled = false;
+            Light4 = false;
+            lightList[2].enabled = true;
+            Light3 = true;
+        }
+      
+    }
+
+    public void Activation4()
+    {
+        if(Activate4==true)
+        {
+            Activate4 = false;
+            lightList[2].enabled = true;
+            Light3 = true;
+            lightList[0].enabled = false;
+            Light1 = false;
+        }
+        else
+        {
+            Activate4 = true;
+            lightList[2].enabled = false;
+            Light3 = false;
+            lightList[0].enabled = true;
+            Light1 = true;
+        }
+       
+
     }
 }
