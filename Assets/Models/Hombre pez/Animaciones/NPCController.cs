fee0d618fdc6c13cc9472cf271a6eaaed0a5246d
Elliot Reyes <gametraxworks@gmail.com>
2023-11-12 00:07:16 -0600

    Agregando lobby y monster 1 /3

---
+++ Assets/Models/Hombre pez/Animaciones/NPCController.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    public Transform[] waypoints;  
+    public float moveSpeed = 3f;   
+    public float rotationSpeed = 5f; 
+    public float waitTime = 2f;    
+    public Animator animator;     
+
+    private int currentWaypointIndex = 0;
+    private float waitTimer = 0f;
+    private bool isWaiting = false;  
+
+    void Start()
+    {
+        currentWaypointIndex = Random.Range(0, waypoints.Length);
+        MoveToWaypoint();
+    }
+
+    void Update()
+    {
+        if (isWaiting)
+        {
+            SetShoutAnimation(true);
+
+            waitTimer -= Time.deltaTime;
+            if (waitTimer <= 0f)
+            {
+                isWaiting = false;
+                SetShoutAnimation(false);
+                SwitchWaypoint();  
+                MoveToWaypoint();  
+            }
+        }
+
+        else
+        {
+            MoveToWaypoint();
+        }
+    }
+
+    void MoveToWaypoint()
+    {
+        Vector3 direction = waypoints[currentWaypointIndex].position - transform.position;
+
+        Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].position, moveSpeed * Time.deltaTime);
+        SetWalkAnimation(true);
+
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f && !isWaiting)
+        {
+            SetWalkAnimation(false);
+            StartWaitTimer();
+        }
+    }
+
+    void StartWaitTimer()
+    {
+        waitTimer = waitTime;
+        isWaiting = true;
+    }
+
+    void SwitchWaypoint()
+    {
+        int newWaypointIndex;
+        do
+        {
+            newWaypointIndex = Random.Range(0, waypoints.Length);
+        } while (newWaypointIndex == currentWaypointIndex);
+
+
+
+        currentWaypointIndex = newWaypointIndex;
+    }
+
+
+
+    void SetWalkAnimation(bool isWalking)
+    {
+        if (animator != null)
+        {
+            animator.SetBool("Walk", isWalking);  
+        }
+    }
+
+    void SetShoutAnimation(bool isShouting)
+    {
+        if (animator != null)
+        {
+            animator.SetBool("Shout", isShouting);  
+        }
+    }
+}
